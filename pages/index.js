1742155ebb47bd004787fda8682bbb19eac1b16a
Abe Porschet <abeporschet@dhcp-130-58-229-120.swarthmore.edu>
2023-09-11 08:37:45 -0400

    fixed silly little typo in description

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Hi, I'm Abe, a statistics and computer science student at Swarthmore College]</p>
+        <p>Hi, I'm Abe, a statistics and computer science student at Swarthmore College</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
